f21c76f15338f57ca7b8f14f4ccbff36b8cc2fde
xinmin <lixinmin@baidu.com>
2016-10-11 18:11:35 +0800

    First commit

---
+++ BMFrame/BMFrame/BMTools/BMTools.h
@@ -0,0 +1,13 @@
+//
+//  BMTools.h
+//  BMFrame
+//
+//  Created by xinmin on 2016/10/10.
+//  Copyright © 2016年 xinmin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BMTools : NSObject
+
+@end
